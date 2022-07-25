e1e98be37af0329e2a5335545e070a2fbdcaf55e
kuzyi <aleckuzma@gmail.com>
2022-07-25 14:52:16 -0400

    added example/testing react code

--- isak/src/App.js
+++ isak/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import boatsArray from './data/boats'
+import './styles/App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Nav from './components/Nav'
+import Listings from './components/Listings'
+import BoatDetails from './components/BoatDetails'
+import BoatForm from './components/BoatForm'
 
-function App() {
+const App = () => {
+  // The boatsArray is passed into state as the initial state for 'boats' in App.js
+  const [boats, setBoats] = useState(boatsArray)
+  const [newBoat, setNewBoat] = useState({
+    id: '',
+    name: '',
+    img: '',
+    description: '',
+    price: ''
+  })
+
+  const addBoat = (e) => {
+    e.preventDefault()
+    const currentBoats = boats
+    const createdBoat = {
+      ...newBoat,
+      id: parseInt(boats.length + 1),
+      price: parseInt(newBoat.price)
+    }
+    currentBoats.push(createdBoat)
+    setBoats(currentBoats)
+    setNewBoat({ id: '', name: '', img: '', description: '', price: '' })
+  }
+
+  const handleChange = (e) => {
+    setNewBoat({ ...newBoat, [e.target.name]: e.target.value })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Nav />
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="listings" element={<Listings boats={boats} />} />
+          <Route path="/listings/:id" element={<BoatDetails boats={boats} />} />
+          <Route
+            path="new"
+            element={
+              <BoatForm
+                newBoat={newBoat}
+                handleChange={handleChange}
+                addBoat={addBoat}
+              />
+            }
+          />
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ isak/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Nav = (props) => {
+  
+  return (
+    <nav className="navbar">
+      <h4>Starboard</h4>
+      <div>
+      <Link to="/">Home</Link>
+      <Link to="listings">Listings</Link>
+      <Link to="new">Add Boat</Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
