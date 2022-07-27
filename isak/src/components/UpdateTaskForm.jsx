85d44c4b7dc921d140f144111480890f17c2f22e
kuzyi <aleckuzma@gmail.com>
2022-07-27 15:51:02 -0400

    able to click the update button and an update component appear

--- isak/src/components/UpdateTaskForm.jsx
+++ isak/src/components/UpdateTaskForm.jsx
@@ -1,31 +1,31 @@
-// import { useState, useEffect } from 'react'
-// import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 
 
 
-// const UpdateTaskForm= (props) => {
+const UpdateTaskForm= (props) => {
 // const [currentUpdate, setCurrentUpdate] = useState([])
 
 
-// let navigate = useNavigate()
+let navigate = useNavigate()
 
-// const handleSubmit = (e) => {
-//   props.addUpdate(e)
-//   navigate('/tasks')
-// }
+const handleSubmit = (e) => {
+  props.addUpdate(e)
+  navigate('/tasks')
+}
 
-//   return(
-//     <div>
-//     <h4>Update Task</h4>
-//     <form onSubmit={ handleSubmit }>
-//       <input type="text" value={currentUpdateTask.updateTaskName} onChange={ props.handleChangeUpdateTask} name={'updateTaskName'} placeholder={'Update Task'} />
-//       <input type="text" value={currentUpdateTask.updateStatus} onChange={ props.handleChangeTaskUpdateTask} name={'updateStatus'} placeholder={'Update Status'} />
-//       <button>Submit Update</button>
-//     </form>
-//     </div>
-//   )
-// }
+  return(
+    <div>
+    <h4>Update Task</h4>
+    <form onSubmit={ handleSubmit }>
+      <input type="text" value={props.currentUpdateTask.updateTaskName} onChange={ props.handleChangeUpdateTask} name={'updateTaskName'} placeholder={'Update Task'} />
+      <input type="text" value={props.currentUpdateTask.updateStatus} onChange={ props.handleChangeTaskUpdateTask} name={'updateStatus'} placeholder={'Update Status'} />
+      <button>Submit Update</button>
+    </form>
+    </div>
+  )
+}
 
-// export default UpdateTaskForm
+export default UpdateTaskForm
